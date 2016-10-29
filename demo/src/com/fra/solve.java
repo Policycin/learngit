ecdb72fe0a20d61b22db68b8af7912a894500301
Policycin <631637682@qq.com>
2016-10-29 17:21:45 +0800

    commit

---
+++ demo/src/com/fra/solve.java
@@ -0,0 +1,14 @@
+package com.fra;
+
+public class solve {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			System.out.println("Hello world!");
+	}
+
+}
